5b69a1497d21bd9ef85211b3fd64dfada04b282c
alhasympratanto372-cmyk <alhasym.pratanto372@gmail.com>
2025-08-16 11:32:41 +0700

    Update home.js
    Mengatur agar navbar responsive di smartphone

--- js/home.js
+++ js/home.js
@@ -46,3 +46,16 @@ function slideNext() {
 
 // Jalankan fungsi geser setiap 3 detik
 setInterval(slideNext, 3000);
+
+
+
+// === Navbar toggle dengan JS murni ===
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.getElementById("navbarToggle"); // tombol hamburger
+  const navMenu = document.getElementById("navbarNavAltMarkup"); // menu nav
+
+  toggleBtn.addEventListener("click", function () {
+    navMenu.classList.toggle("show"); // tambahkan/hapus class show
+  });
+});
+
