38b1530bc078b646af513025cafc81a52259e66b
alhasympratanto372-cmyk <alhasym.pratanto372@gmail.com>
2025-08-16 12:22:14 +0700

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -29,3 +29,15 @@ contactForm.addEventListener('submit', function(event) {
     // Reset form
     contactForm.reset(); // Mengosongkan semua input
 });
+
+
+
+// === Navbar toggle ===
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.getElementById("navbarToggle"); // tombol hamburger
+  const navMenu = document.getElementById("navbarNavAltMarkup"); // menu nav
+
+  toggleBtn.addEventListener("click", function () {
+    navMenu.classList.toggle("show"); // tambahkan/hapus class show
+  });
+});
